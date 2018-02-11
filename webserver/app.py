970d21fd83420c25f23d4a7557cfecc0ebd40836
giedroyc <rgiedroyc@gmail.com>
2018-02-11 19:02:46 +0000

    rewrite

--- webserver/app.py
+++ webserver/app.py
@@ -1,18 +1,19 @@
 from aiohttp import web
 import handlers
+import asyncio
 
 def setup_routes(app):
     app.router.add_get('/', handlers.helloworld)
     app.router.add_get('/transactions/', handlers.helloworld)
     app.router.add_get('/hello/{name}', handlers.helloworld)
     app.router.add_get('/ws/', handlers.helloworld)
+    app.router.add_get('/redisadd/', handlers.redisadd)
 
 
 def main():
     app = web.Application()
     setup_routes(app)
-    web.run_app(app, host='127.0.0.1', port=8888)
-
+    web.run_app(app, host='127.0.0.1', port=8888, loop=asyncio.get_event_loop())
 
 if __name__ == "__main__":
     main()
--- webserver/handlers.py
+++ webserver/handlers.py
@@ -1,6 +1,18 @@
 from aiohttp import web
+import asyncio
+import aioredis
 
 async def helloworld(request):
     name = request.match_info.get('name', "Anonymous")
     text = "Hello, " + name
     return web.Response(text=text)
+
+async def redisadd():
+    conn = await aioredis.create_connection(
+        'redis://localhost', loop=asyncio.get_event_loop())
+    await conn.execute('set', 'my-key', 'value')
+    val = await conn.execute('get', 'my-key')
+    print(val)
+    conn.close()
+    await conn.wait_closed()
+    print('Connection closed')
